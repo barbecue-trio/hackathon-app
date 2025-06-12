a16c7a56f37eb984202239585fe2e496f8efeb29
taco-tortilla <tortillataco0@gmail.com>
2025-06-12 19:07:22 +0900

    フォーマットの適用

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { getAnalytics } from "firebase/analytics"
+import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,10 +9,10 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
+}
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
-export default app;
-export { analytics };
+export default app
+export { analytics }
