893dd0cfd606ef46ab01318406c62991878e721f
hond0413 <hondsoccer0413@gmail.com>
2025-06-21 22:52:44 +0900

    feat: add page components for routing
    - Add Home page component
    - Add DietaryRestrictions page component
    - Add MenuScanner page component
    - Add Menu page component
    - Add MenuDetail page component
    - Add Settings page component
    
    Each page component contains basic structure with placeholder content.

---
+++ src/pages/DietaryRestrictions.tsx
@@ -0,0 +1,9 @@
+function DietaryRestrictions() {
+  return (
+    <div>
+      <h1>Dietary Restrictions</h1>
+    </div>
+  )
+}
+
+export default DietaryRestrictions
---
+++ src/pages/MenuScanner.tsx
@@ -0,0 +1,9 @@
+function MenuScanner() {
+  return (
+    <div>
+      <h1>Menu Scanner</h1>
+    </div>
+  )
+}
+
+export default MenuScanner
