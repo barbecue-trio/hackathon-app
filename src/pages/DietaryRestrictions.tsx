4daaa3173b078621a6c7c55f2534e0244ab17d92
taco-tortilla <tortillataco0@gmail.com>
2025-06-24 15:35:26 +0900

    ユーザーの食事制限情報をローカルストレージに保存する機能の追加

--- src/pages/DietaryRestrictions.tsx
+++ src/pages/DietaryRestrictions.tsx
@@ -1,7 +1,67 @@
+import {
+  Button,
+  Checkbox,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material"
+import { Fragment, useState } from "react"
+import { dietaryRestrictionGroups } from "../data/dietaryRestrictionGroups"
+import type { UserDietaryRestrictions } from "../types/dietaryRestrictions"
+
 function DietaryRestrictions() {
+  const [userDietaryRestrictions, setUserDietaryRestrictions] = useState<UserDietaryRestrictions>({
+    allergies: [],
+    religiousRestrictions: [],
+    otherRestrictions: [],
+  })
+
+  const handleToggle = (groupKey: keyof UserDietaryRestrictions, option: string) => {
+    setUserDietaryRestrictions((prev) => {
+      const currentGroup = prev[groupKey]
+      const newGroup = currentGroup.includes(option)
+        ? currentGroup.filter((item) => item !== option)
+        : [...currentGroup, option]
+
+      return { ...prev, [groupKey]: newGroup }
+    })
+  }
+
+  const handleSave = () => {
+    localStorage.setItem("dietaryRestrictions", JSON.stringify(userDietaryRestrictions))
+  }
+
   return (
     <div>
       <h1>Dietary Restrictions</h1>
+      <List>
+        {dietaryRestrictionGroups.map((group) => (
+          <Fragment key={group.key}>
+            <Typography
+              variant="h3"
+              sx={{ textAlign: "left" }}
+              fontSize={18}
+              fontWeight="bold"
+              my={2}
+            >
+              {group.label}
+            </Typography>
+            {group.options.map((option) => (
+              <ListItemButton key={option} onClick={() => handleToggle(group.key, option)}>
+                <ListItemIcon>
+                  <Checkbox checked={userDietaryRestrictions[group.key].includes(option)} />
+                </ListItemIcon>
+                <ListItemText primary={option} />
+              </ListItemButton>
+            ))}
+          </Fragment>
+        ))}
+      </List>
+      <Button type="button" onClick={handleSave}>
+        save
+      </Button>
     </div>
   )
 }
