1ca56a7cccca5c5826b5825254b6eb762d050fbd
taco-tortilla <tortillataco0@gmail.com>
2025-06-28 13:39:35 +0900

    feat: メニュー画像生成エンドポイントを追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 // エンドポイント別のindex.tsファイルからエクスポート
-export { processMenuImage } from "./endpoints/process-menu-image"
-export { generateFoodCulture } from "./endpoints/generate-food-culture"
-export { generateCategories } from "./endpoints/generate-categories"
-export { testProcessMenuImage } from "./endpoints/test-process-menu-image"
+export { processMenuImage } from './endpoints/process-menu-image';
+export { generateFoodCulture } from './endpoints/generate-food-culture';
+export { generateCategories } from './endpoints/generate-categories';
+export { generateMenuImage } from './endpoints/generate-menu-image';
+export { testProcessMenuImage } from './endpoints/test-process-menu-image';
