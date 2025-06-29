a1728e4172a6446c00e4338a4671a0966f7f011d
hond0413 <hondsoccer0413@gmail.com>
2025-06-29 14:15:56 +0900

    feat: functions層の統合型定義基盤設定を追加
    - functions/src/types.tsでプロジェクトルート型定義を再エクスポート
    - functions/tsconfig.jsonでtypeRootsとinclude設定を最適化
    - 統合型定義使用の基盤を整備

---
+++ functions/src/types.ts
@@ -0,0 +1,11 @@
+// プロジェクトルートの型定義を再エクスポート
+export type { MenuItem, MenuCollection } from "../../types/menu";
+export type { GeneratedImage } from "../../types/image";
+export type {
+  ProcessImageRequest,
+  ProcessImageResponse,
+} from "../../types/api";
+export type {
+  GenerateFoodCultureRequest,
+  GenerateFoodCultureResponse,
+} from "../../types/api";
