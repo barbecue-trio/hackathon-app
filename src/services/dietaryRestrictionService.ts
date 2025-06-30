5aebe04f73d1de95d84fb07b628eb66e1cc9a294
taco-tortilla <tortillataco0@gmail.com>
2025-06-30 19:35:59 +0900

    feat: 不要なコードを削除

--- src/services/dietaryRestrictionService.ts
+++ src/services/dietaryRestrictionService.ts
@@ -3,23 +3,6 @@ import { religiousRestrictionList } from "@/data/religiousRestrictions"
 import type { AllergenId, ReligiousRestrictionId } from "@/types"
 import { getDietaryRestrictions } from "@/utils/localStorage"
 
-// export function getInitDietaryRestrictions() {
-//   const storage = getDietaryRestrictions()
-//   console.log(storage)
-//   const allergies = allergyList.map((item) => ({
-//     ...item,
-//     checked: storage.allergies.includes(item.id),
-//   }))
-//   const religious = religiousRestrictionList.map((item) => ({
-//     ...item,
-//     checked: storage.religious.includes(item.id),
-//   }))
-//   return {
-//     allergies,
-//     religious,
-//   }
-// }
-
 export function getInitDietaryRestrictions() {
   const storage = getDietaryRestrictions()
 
